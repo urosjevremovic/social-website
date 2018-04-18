b64fd573976a209d585ade6c6bd42a032d4f317c
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-04-18 19:54:22 +0200

    modified social authentication pipeline and added README and LICENSE files

---
+++ README.rst
@@ -0,0 +1,5 @@
+Picturest
+-------------
+
+Social website for liking and sharing pictures with other people.
+
--- social_website/settings/production.py
+++ social_website/settings/production.py
@@ -130,6 +130,20 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.twitter.TwitterOAuth',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 SOCIAL_AUTH_FACEBOOK_KEY = '147172969320175'
 SOCIAL_AUTH_FACEBOOK_SECRET = '9f7c5a3d6d6f93e7a91975bc4de7f6ea'
 
