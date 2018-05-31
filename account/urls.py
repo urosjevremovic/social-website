f36a26a79cf5bfc7c4c1924762c27f54c88ea37f
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-05-31 16:18:05 +0200

    fixed a url bug that was causing user to be redirected to login page after password change instead of password_change_done url

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import (logout, login, logout_then_login,
-                                       password_change_done, password_reset, password_reset_done,
-                                       password_reset_complete, password_reset_confirm, password_change)
+from django.contrib.auth.views import (logout, login, logout_then_login, )
 
 from . import views
 
@@ -13,8 +11,6 @@ urlpatterns = [
     path('activate/<code>/', views.activate_user_view, name='activate'),
     path('edit/', views.edit, name='edit'),
     path('logout_then_login/', logout_then_login, name='logout_then_login'),
-    path('password_change/', views.login_after_password_change, name='password_change'),
-    path('password_change/done/', password_change_done, name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
     path('users/', views.user_list, name='user_list'),
     path('users/follow/', views.user_follow, name='user_follow'),
